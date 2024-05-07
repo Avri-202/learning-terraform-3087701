8685fe5b0c873422870bcfce043044b507b1c563
Avri-202 <76390984+Avri-202@users.noreply.github.com>
2024-05-07 17:07:54 +0300

    Fix VPC

--- main.tf
+++ main.tf
@@ -29,6 +29,13 @@ vpc_security_group_ids = [aws_security_group.web.id]
   }
 }
 
+resource "aws_security_group" "web" {
+  name = "web"
+  description = "Allow http and https in. Allow everything out.
+
+  vpc_id = data.aws_vpc.default.id
+}
+
 resource "aws_security_group_rule" "web_http_in" {
   type        = "ingress"
   from_port   =  80
