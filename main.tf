93fde56640d11b5f173905eedea9e73fadcf9526
Avri-202 <76390984+Avri-202@users.noreply.github.com>
2024-05-07 16:56:12 +0300

    Fix SG

--- main.tf
+++ main.tf
@@ -22,10 +22,10 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-vpc_security_group_ids = [aws_security_group_web.id]
+vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "LearningTerraform"
   }
 }
 
@@ -36,7 +36,7 @@ resource "aws_security_group_rule" "web_http_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_web.id
+  security_group_id = aws_security_group.web.id
 }
 
 resource "aws_security_group_rule" "web_https_in" {
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "web_https_in" {
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_web.id
+  security_group_id = aws_security_group.web.id
 }
 
 resource "aws_security_group_rule" "web_everything_out" {
@@ -56,5 +56,5 @@ resource "aws_security_group_rule" "web_everything_out" {
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
-  security_group_id = aws_security_group_web.id
+  security_group_id = aws_security_group.web.id
 }
